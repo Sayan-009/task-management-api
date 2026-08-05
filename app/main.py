9100f7df58e878164ad5ea08bc92ee321e14e493
Sayan-009 <sayankhanra019@gmail.com>
2026-08-05 19:56:50 +0530

    add user router in main file

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
 
-# from app.users.router import router as user_router
+from app.users.router import router as user_router
 
 app = FastAPI()
 
-# app.include_router(user_router)
+app.include_router(user_router)
 
 @app.get('/health')
 def health():
